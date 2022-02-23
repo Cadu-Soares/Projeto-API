35ec3e46ed3067a9f8b259e0320c4520f4edb697
Cadu Soares <devcadu.soares@outlook.com>
2022-02-23 17:05:02 -0300

    Atualização do Projeto

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+require('./controllers/authController')(app);
 
-app.get('/', (req, res) =>{
-  res.send("Hello World!");
-});
-
+// app.get('/', (req, res) =>{
+//   res.send("Hello World!");
+// });
 
 app.listen(3000);
