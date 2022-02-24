db87bbf24847636aa6b683ffc9e4024e11b0b1f9
Cadu Soares <devcadu.soares@outlook.com>
2022-02-24 10:47:14 -0300

    atualização do projeto

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const config = require('../config/auth');
+
 mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
 
 mongoose.Promise = global.Promise;
